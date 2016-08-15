0e72196a1304801b309890d05b0a10d6c3bf3ec2
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-08-15 20:28:08 +0300

    added second test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-testing
 How to test react applications
+
+## How to
+1. We are using `react-addons-test-utils` a util library from Facebook.
+2. Move functions from components to separate modules - it makes easier testing them.
---
+++ src/createId.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+import createId from './createId';
+
+describe('createId', () => {
+  it('it should convert a description into a unique Id', () => {
+    const actual = createId(123, 'Cool example description');
+    const expected = '123-cool-example';
+    expect(actual).toEqual(expected);
+  });
+});
