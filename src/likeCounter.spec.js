8a912e165eef68a1bc7255220c36801c75e11297
rafaelkyrdan <rafaelkyrdan@gmail.com>
2016-08-16 13:33:23 +0300

    Added example of test which checking the type of component

---
+++ src/likeCounter.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+
+const LikeCounter = ({greeting}) => (
+  <a>click on me</a>
+);
+
+describe('LikeCounter', () => {
+
+  it('it should type "a"', () => {
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<LikeCounter />);
+    const actual = renderer.getRenderOutput().type;
+    const expected = 'a';
+    expect(actual).toEqual(expected);
+  });
+
+});
